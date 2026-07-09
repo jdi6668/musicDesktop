939de899a1c6a05c4ceb54418da1e6feca24dae4
jj <8@qq.com>
2026-07-09 22:54:22 +0800

    jj commit at 2026-07-09 22:54:21

--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"embed"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
 	"os"
-	"path/filepath"
 
 	"github.com/yuaotian/go-win-webview2"
 )
+
+//go:embed index.html heisemaoyi.mp3 heisemaoyi.lrc favicon.ico
+var assets embed.FS
 
 // findFreePort 获取一个可用的本地端口
 func findFreePort() (int, error) {
@@ -26,19 +29,6 @@ func findFreePort() (int, error) {
 }
 
 func main() {
-	// 获取可执行文件所在目录，作为静态资源根目录
-	exePath, err := os.Executable()
-	if err != nil {
-		log.Fatalf("无法获取可执行文件路径: %v", err)
-	}
-	rootDir := filepath.Dir(exePath)
-
-	// 确认 index.html 存在
-	indexPath := filepath.Join(rootDir, "index.html")
-	if _, err := os.Stat(indexPath); os.IsNotExist(err) {
-		log.Fatalf("未找到 index.html，请确保它与可执行文件在同一目录: %s", indexPath)
-	}
-
 	// 获取可用端口
 	port, err := findFreePort()
 	if err != nil {
@@ -47,18 +37,19 @@ func main() {
 	addr := fmt.Sprintf("127.0.0.1:%d", port)
 	url := fmt.Sprintf("http://%s", addr)
 
-	// 静态文件服务器
-	fs := http.FileServer(http.Dir(rootDir))
-	http.Handle("/", fs)
+	// 从嵌入的资源提供静态文件
+	http.Handle("/", http.FileServer(http.FS(assets)))
 
 	// 启动 HTTP 服务器（在 goroutine 中）
 	go func() {
 		log.Printf("服务器已启动: %s", url)
-		log.Printf("资源目录: %s", rootDir)
 		if err := http.ListenAndServe(addr, nil); err != nil {
 			log.Fatalf("服务器错误: %v", err)
 		}
 	}()
+
+	// 设置 WebView2 浏览器启动参数，允许自动播放
+	os.Setenv("WEBVIEW2_ADDITIONAL_BROWSER_ARGUMENTS", "--autoplay-policy=no-user-gesture-required")
 
 	// 创建 WebView2 全屏窗口
 	w := webview2.NewWithOptions(webview2.WebViewOptions{
